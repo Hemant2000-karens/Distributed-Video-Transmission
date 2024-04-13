7e3cf5cb0dcd6bb77616f02257aed93fdd4ba0ec
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2024-04-13 09:29:59 +0530

    fixed video bugs

--- dvs/settings.py
+++ dvs/settings.py
@@ -136,6 +136,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dvs_app', 'static'),
 ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media','videos')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- dvs/urls.py
+++ dvs/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from dvs_app import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.show_broadcast, name='show_broadcast/'),
-    #path('..','media/videos/',name=videos),
-]
+    path('', views.show_broadcast, name='show_broadcast'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # print("BASE_DIR:", path('show_broadcast/'))
