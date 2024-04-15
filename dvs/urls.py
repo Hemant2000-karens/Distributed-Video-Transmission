0c304bde70ac69065498294cc238bb738ad70e91
Hemant Kumar <58652350+Hemant2000-karens@users.noreply.github.com>
2024-04-15 15:20:06 +0530

    updated and fix

--- dvs/urls.py
+++ dvs/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from dvs_app import views
-
+from dvs_app.views import login_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_view, name='login'),
     path('', views.show_broadcast, name='show_broadcast'),
 ]
 
--- dvs_app/views.py
+++ dvs_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 from django.utils import timezone
@@ -9,6 +9,22 @@ from .models import BroadcastingInfo
 #     current_time = timezone.now()
 #     broadcast_info = BroadcastingInfo.objects.first()  # Fetch broadcasting info from database
 #     return render(request, 'show_broadcast.html', {'current_time': current_time, 'broadcast_info': broadcast_info})
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to home page after successful login
+        else:
+            error_message = 'Invalid username or password. Please try again.'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
 
 
 def show_broadcast(request):
