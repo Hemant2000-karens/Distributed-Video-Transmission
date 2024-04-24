2e91faf997e3271c4214e779dc9d01c7a122ea06
Hemant Kumar <team.karensbook@icloud.com>
2024-04-25 04:58:27 +0530

    fixed username issue

--- dvs_app/middleware.py
+++ dvs_app/middleware.py
@@ -12,9 +12,12 @@ class ClientInteractionMiddleware:
         # Capture client interactions
         ip_address = request.META.get('REMOTE_ADDR')
         action = request.path_info  # Assuming path_info represents the action
-        ClientInteractionLog.objects.create(client_ip=ip_address, action=action)
-        
-        # Pass the request to the next middleware or view
+
+        if request.user.is_authenticated:
+            username = request.user.username
+        else:
+            username = "Anonymous" 
+        ClientInteractionLog.objects.create(client_ip=ip_address, action=action, username=username)
         response = self.get_response(request)
         return response
 
